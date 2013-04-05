9219f94e248bba83a8d67af60661c3655dc2c97b
Bustedblah <Luis.F.Carrio@gmail.com>
2013-04-04 22:04:06 -0700

    Everything is now in a folder

--- json_strip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Strip JSON structure from URL results file
-# usage is:
-# json_strip input_file
-# Input file will be assumed to have extension .res and output .url
-grep link $1.res | awk '{ print $2 }' | cut -f 2 -d \" > $1.url
--- merger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 2 ]
-then
-    echo Need two arguments 
-    exit
-fi
-(cat $1;cat $2) | sort | uniq > /tmp/tmp
-mv /tmp/tmp $1
