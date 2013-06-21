88e164146fbc90c3382ad54374023fcef150e642
gardner <gbickfor@adobe.com>
2013-06-20 21:18:15 -0700

    Adding has_many to model for award and description.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,8 @@
 class Challenge < ActiveRecord::Base
   attr_accessible :award, :deadline, :description, :image_url, :name, :post_date, :rules, :url, :xpath
   validates :name, :presence => true
+  has_many :award
+  has_many :deadline
   
   searchable do
 	  text :name, :stored => true
