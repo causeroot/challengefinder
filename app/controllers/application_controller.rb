e0f1108ef80eed39a5d2e156e4478418023dd4db
gardner <gbickfor@adobe.com>
2013-07-23 20:52:47 -0700

    Updated redirect to something useful

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_filter :require_login
   protect_from_forgery
+  protected
+
+  def not_authenticated
+    redirect_to login_url, :alert => "Please login first."
+  end
+  
 end
