c34d87e041dd356fd669812dd0b09ae427b9a38f
Bustedblah <Luis.F.Carrio@gmail.com>
2013-09-29 13:58:59 -0700

    fixed up numberic_value and index deadline

--- db/migrate/20130816031555_update_challenge_content_with_status.rb
+++ db/migrate/20130816031555_update_challenge_content_with_status.rb
@@ -2,4 +2,8 @@ class UpdateChallengeContentWithStatus < ActiveRecord::Migration
   def up
     add_column :challenges, :status, :string
   end
+
+  def down
+    remove_column :challenges, :status
+  end
 end
