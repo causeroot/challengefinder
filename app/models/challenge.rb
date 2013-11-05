a6fe650a84a889f7bc86491bff356259f023b462
gardner <gbickfor@adobe.com>
2013-11-05 00:06:33 -0800

    Updated challenge deadline date to use a datetime field in the database.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,7 +4,7 @@ class Challenge < ActiveRecord::Base
   accepts_nested_attributes_for :awards, :reject_if => proc { |a| a['value'].blank? and a['description'].blank? }
   accepts_nested_attributes_for :deadlines, :reject_if => proc { |a| a['date'].blank? and a['description'].blank? }
   
-  attr_accessible :url, :title, :status, :tag_line, :summary, :rules, :eligibility, :fee, :numeric_value, :index_deadline, :awards_attributes, :deadlines_attributes, :post_date, :image_url, :sponsor, :contact_info, :topic, :structure, :resultant, :xpath_check, :index_deadline_str
+  attr_accessible :url, :title, :status, :tag_line, :summary, :rules, :eligibility, :fee, :numeric_value, :index_deadline, :index_deadline_str, :awards_attributes, :deadlines_attributes, :post_date, :image_url, :sponsor, :contact_info, :topic, :structure, :resultant, :xpath_check, :index_deadline_str
   validates :title, :presence => true
 
   searchable do
@@ -18,15 +18,23 @@ class Challenge < ActiveRecord::Base
     text :contact_info, :stored => true
   end
 
-  def index_deadline=(i)
+  def due_at_string
+    due_at.to_s(:db)
+  end
+
+  def due_at_string=(due_at_str)
+    self.due_at = Time.parse(due_at_str)
+  end
+  
+  def index_deadline_str=(i)
     self.index_deadline = Chronic.parse(i)
   end
 
-  def index_deadline
-    if self.index_deadline.nil?
+  def index_deadline_str
+    if index_deadline.nil?
       return 'No Deadline'
     else
-      self.index_deadline.to_str
+      index_deadline.to_date.to_formatted_s(:long)
     end
   end
 end
